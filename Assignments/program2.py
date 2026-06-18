9af9b1d471aae097ef3979cdb1fd4d428bcf3009
shravani-nawalkar <nawalkar.shravani@gmail.com>
2026-06-19 00:19:48 +0530

    Add files via upload

---
+++ Assignments/program2.py
@@ -0,0 +1,18 @@
+print("Enter First Number:")
+no1 = int(input())
+
+print("Enter Second Number:")
+no2 = int(input())
+
+Ans = no1 + no2
+Ans1 = no1 * no2
+Ans2 = no1 - no2
+Ans3 = no1 // no2
+
+print("Your Addition is:",Ans)
+
+print("Your Multiplication is is:",Ans1)
+
+print("Your Substraction is is:",Ans2)
+
+print("Your Division is:",Ans3)
---
+++ Assignments/program3.py
@@ -0,0 +1,11 @@
+print("Enter Your Name:")
+
+Name = input()
+
+print("Enter your Age:")
+
+Age =int(input())
+
+Age = Age + 1
+
+print("Hello..!!",Name, "You Will turn" ,Age,"This year")
